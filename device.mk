c6b021f5069a02cfd6b478a84c0061e8c6615d06
Adam Farden <adam@farden.cz>
2015-11-08 13:06:52 +0100

    piccolo: Stop the QC-QMI message spam
    Bro do you even logcat?
    
    Change-Id: Idc308e2c22cf94389f12bf6d798fa85eab575c5b
    Signed-off-by: Adam Farden <adam@farden.cz>

--- device.mk
+++ device.mk
@@ -213,6 +213,7 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.qmi.adb_logmask=0 \
     persist.timed.enable=true \
     ro.qualcomm.cabl=2 \
     ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
