08a675e47fa2d3cda5a14234c59529e91c180602
Christian Morlok <ChristianMorlok@gmail.com>
2015-11-26 15:20:55 +0100

    piccolo: Inherit from msm8939-common
    Change-Id: I205f1d161da2f8c0818480eed568c5c12ccc3d15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,22 +14,30 @@
 # limitations under the License.
 #
 
+include device/cyanogen/msm8939-common/BoardConfigCommon.mk
+
+LOCAL_PATH := device/bq/piccolo
+
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := msm8916
-TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
-# Platform
-TARGET_BOARD_PLATFORM := msm8916
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno405
-
-# Architecture
+# Overwrite architecture from msm8939-common since we don't have 64 bit support
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
+TARGET_2ND_ARCH :=
+TARGET_2ND_ARCH_VARIANT :=
+TARGET_2ND_CPU_ABI :=
+TARGET_2ND_CPU_ABI2 :=
+TARGET_2ND_CPU_VARIANT :=
+
+TARGET_BOARD_SUFFIX := _32
+TARGET_USES_64_BIT_BINDER := false
+
+# Compiler
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
@@ -37,13 +45,11 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_SOURCE := kernel/bq/piccolo
 TARGET_KERNEL_CONFIG := cyanogenmod_piccolo_defconfig
 TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_HEADER_ARCH := arm
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_SEPARATED_DT := true
 BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_DTBTOOL_ARGS := -2
+TARGET_KERNEL_CROSS_COMPILE_PREFIX :=
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := aquarism5,piccolo
@@ -52,17 +58,10 @@ TARGET_OTA_ASSERT_DEVICE := aquarism5,piccolo
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-smd"
 
 # Audio
-BOARD_USES_ALSA_AUDIO := true
-AUDIO_FEATURE_DEEP_BUFFER_RINGTONE := true
-AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
-AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
 USE_CUSTOM_AUDIO_POLICY := 1
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/bq/piccolo/bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-BLUETOOTH_HCI_USE_MCT := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 # Camera
 COMMON_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
@@ -71,13 +70,7 @@ COMMON_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 
 # CMHW
-BOARD_HARDWARE_CLASS := device/bq/piccolo/cmhw/src
-
-# Add suffix variable to uniquely identify the board
-TARGET_BOARD_SUFFIX := _32
-
-# Encryption
-TARGET_HW_DISK_ENCRYPTION := true
+BOARD_HARDWARE_CLASS += $(LOCAL_PATH)/cmhw/src
 
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x02000000
@@ -91,32 +84,9 @@ BOARD_FLASH_BLOCK_SIZE             := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 # FM
 TARGET_FM_LEGACY_PATCHLOADER := true
 
-# Fonts
-EXTENDED_FONT_FOOTPRINT := true
-
 # GPS
 TARGET_GPS_HAL_PATH := $(COMMON_PATH)/gps
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm8916
-
-# Graphics
-TARGET_USES_C2D_COMPOSITION := true
-TARGET_USES_ION := true
-USE_OPENGL_RENDERER := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
-OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
-
-# Shader cache config options
-# Maximum size of the GLES Shaders that can be cached for reuse.
-# Increase the size if shaders of size greater than 12KB are used.
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-
-# Maximum GLES shader cache size for each app to store the compiled shader
-# binaries. Decrease the size if RAM or Flash Storage size is a limitation
-# of the device.
-MAX_EGL_CACHE_SIZE := 2048*1024
-
-# Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # Keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
@@ -124,48 +94,19 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# Memory
-MALLOC_IMPL := dlmalloc
-
-# Power
-TARGET_POWERHAL_VARIANT := qcom
-
-# QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
-
 # Recovery
-TARGET_RECOVERY_FSTAB := device/bq/piccolo/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 COMMON_GLOBAL_CFLAGS += -DRECOVERY_FONT='"roboto_15x24.h"'
-
-# RIL
-TARGET_RIL_VARIANT := caf
-PROTOBUF_SUPPORTED := true
+TARGET_RECOVERY_UPDATER_LIBS :=
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/bq/piccolo/sepolicy
-
-# Time services
-BOARD_USES_QC_TIME_SERVICES := true
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 32
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-
-# Wifi
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HAS_QCOM_WLAN_SDK := true
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_FW_PATH_AP := "ap"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
 -include vendor/bq/piccolo/BoardConfigVendor.mk
